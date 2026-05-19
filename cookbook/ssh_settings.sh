3459ea676e77b3330db2300e18ab3da31f9b7f6a
Vladislav Dragushin <maxhc@inbox.ru>
2026-05-19 21:05:35 +0300

    Rename UTILS

---
+++ cookbook/ssh_settings.sh
@@ -0,0 +1,22 @@
+# Базовая настройка сервера
+
+sudo apt update && sudo apt upgrade -y &
+sudo echo "localhost 127.0.0.1" > /etc/hosts
+sudo apt install openssh-server -y &
+sudo systemctl start ssh
+sudo systemctl enable ssh
+sudo systemctl status ssh
+ss -antlp # проверяем порты
+ssh-copy-id user@ip_address
+sudo nano /etc/ssh/sshd_config
+# Меняем порт
+# PermitRootLogin no
+# PubkeyAuthentication yes
+#PasswordAuthentication no
+sudo systemctl stop ssh.socket
+sudo systemctl disable ssh.socket
+sudo systemctl restart sshd
+
+# включаем фаервол
+sudo ufw allow 31200/tcp
+sudo ufw enable
